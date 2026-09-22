673639827d9916a406e93acd07c98b70a2ec2207
yager <danila.kirillov@list.ru>
2026-09-22 21:25:26 +0400

    deleted mistake landing

--- landing/src/data/seo.ts
+++
@@ -1,7 +0,0 @@
-export const seo = {
-  /** TODO: заменить на боевой домен (синхронно с index.html, robots.txt, sitemap.xml). */
-  siteUrl: 'https://intevrum.ru/',
-  siteName: 'INTEVRUM Group',
-  organizationName: 'INTEVRUM Group',
-  locale: 'ru_RU',
-};
